dc2a98c2c169a4a0de0e29ba7f59b27cd9f5cb4d
amirabuabas <112530051+amirabuabas@users.noreply.github.com>
2022-09-15 15:12:20 -0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,25 +1,25 @@
 let Movies =[
-{Name: "Me time"
+{ Name: "Me time"
  Released date: "August 26, 2022"
  Director: "John Hamburg"
  Key actor: "Kevin Hart"
 }
-{Name: "Girls Trip"
+{ Name: "Girls Trip"
 Released date: "July 21, 2017"
 Director: "Malcolm D. Lee"
 Key actor: "Regina Hall"
 }
-{Name: "The Last Shift"
+{ Name: "The Last Shift"
 Released date: "September 25, 2020"
 Director: "Andrew Cohn"
 Key actor: "Andrew Cohn"
 }
-{Name: "Mainstream"
+{ Name: "Mainstream"
 Released date: "May 7, 2021"
 Director: "Gia Coppola"
 Key actor: "Andrew Garfield"
 }
-{Name: "Ingrid Goes West"
+{ Name: "Ingrid Goes West"
 Released date: "August 25, 2017"
 Director: "Matt Spicer"
 Key actor: "Aubrey Plaza"
