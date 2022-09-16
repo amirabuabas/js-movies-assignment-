0287ebc42175a6ca611d05db6c085c2b23ab61e9
amirabuabas <112530051+amirabuabas@users.noreply.github.com>
2022-09-15 19:12:55 -0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,29 +1,35 @@
 let Movies =[
-{ Name: "Me time"
- Released date: "August 26, 2022"
- Director: "John Hamburg"
- Key actor: "Kevin Hart"
-}
-{ Name: "Girls Trip"
-Released date: "July 21, 2017"
-Director: "Malcolm D. Lee"
-Key actor: "Regina Hall"
-}
-{ Name: "The Last Shift"
-Released date: "September 25, 2020"
-Director: "Andrew Cohn"
-Key actor: "Andrew Cohn"
-}
-{ Name: "Mainstream"
-Released date: "May 7, 2021"
-Director: "Gia Coppola"
-Key actor: "Andrew Garfield"
-}
-{ Name: "Ingrid Goes West"
-Released date: "August 25, 2017"
-Director: "Matt Spicer"
-Key actor: "Aubrey Plaza"
-}
-
-]console console.log(movies);
+    { 
+        Name : "Me time",
+     releaseDate : "August 26, 2022",
+     Director : "John Hamburg", 
+     KeyActor : "Kevin Hart",
+    } ,
+    { 
+        Name : "Girls Trip", 
+    ReleaseDate : "July 21, 2017", 
+    Director : "Malcolm D. Lee", 
+    KeyActor : "Regina Hall", 
+    } ,
+    { 
+        Name : "The Last Shift", 
+    ReleaseDate : "September 25, 2020", 
+    Director : "Andrew Cohn", 
+    KeyActor : "Andrew Cohn", 
+    } ,
+    { 
+        Name : "Mainstream", 
+    ReleaseDate : "May 7, 2021", 
+    Director : "Gia Coppola",
+    KeyActor : "Andrew Garfield", 
+    } ,
+    { 
+        Name : "Ingrid Goes West", 
+    ReleaseDate : "August 25, 2017", 
+    Director : "Matt Spicer", 
+    KeyActor : "Aubrey Plaza", 
+    }
+    
+    ]
+    console.log(Movies);
 
